e31caeef49e0f6ba3d1ac36fc12056d35737938d
Gerson Enriquez <gerson.enriquez@edreamsodigeo.com>
2024-05-14 18:05:06 +0200

    Add category name, budget and dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { useFormStatus } from "react-dom";
 import action from './actions.js';
 import './App.css'
@@ -11,34 +9,91 @@ function Submit() {
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [monthlyBudget, setMonthlyBudget] = useState(0)
+  const [categories, setCategories] = useState([])
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleSetMonthlyBudget = e => {
+    e.preventDefault()
+    const form = e.target.form
+    const budget = form['monthly-budget'].value
+    setMonthlyBudget(Number(budget))
+  }
+
+  const handleAddCategory = e => {
+    e.preventDefault()
+    const form = e.target.form
+    const categoryName = form['category-name'].value
+    const categoryBudget = form['category-budget'].value
+    setCategories([...categories, { name: categoryName, budget: Number(categoryBudget) }])
+  }
+
+  const handleAddExpense = e => {
+    e.preventDefault()
+    const form = e.target.form
+    const categoryName = form['category-select'].value
+    const categoryQuantity = form['category-quantity'].value
+    console.log({categoryName})
+    console.log(Number(categoryQuantity))
+    setIsOpen(false)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <form action={action}>
-        <Submit />
-      </form>
-    </>
+    <form style={{ display: 'flex', flexDirection: 'column'}} action={action}>
+      <h1>Set up:</h1>
+      <h2>Budget - {monthlyBudget}</h2>
+      <label htmlFor='monthly-budget'>Monthly Budget</label>
+      <input
+        id='monthly-budget'
+        type="number"
+        min={0}
+        name="monthly-budget"
+      />
+      <button onClick={handleSetMonthlyBudget}>Set Budget</button>
+
+      <label htmlFor='category-name'>Category Name</label>
+      <input
+        id='category-name'
+        type="text"
+        name="category-name"
+      />
+      <label htmlFor='category-budget'>Category Budget</label>
+      <input
+        id='category-budget'
+        type="number"
+        min={0}
+        name="category-budget"
+      />
+      <button onClick={handleAddCategory}>Add Category</button>
+      {
+        categories.map((category) => (
+          <p key={category.name}>Category: {category.name} - {category.budget}</p>
+        ))
+      }
+
+      <button onClick={() => setIsOpen(true)}>Aggiungi Spesa</button>
+      <dialog open={isOpen}>
+        <label for="category-select">Scegli la categoria</label>
+        <select name="category" id="category-select">
+          <option value="">--Please choose an option--</option>
+          {
+            categories.map((category) => (
+              <option key={category.name} value={category.name}>{category.name}</option>
+            ))
+          }
+        </select>
+        <label for="quantity">Quantitá</label>
+        <input
+          id='category-quantity'
+          type="number"
+          min={0}
+          name="category-quantity"
+        />
+        <button onClick={handleAddExpense}>Aggiungi</button>
+      </dialog>
+      {/* <Submit /> */}
+    </form>
   )
 }
 
